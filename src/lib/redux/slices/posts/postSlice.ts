7b20ae655255411ade6818013d2ad11407ec7463
Nikita0708 <nreva97@google.com>
2024-08-25 21:15:51 +0300

    return comments to work fine

--- src/lib/redux/slices/posts/postSlice.ts
+++ src/lib/redux/slices/posts/postSlice.ts
@@ -1,232 +1,310 @@
-import { postAPI } from '@lib/API/postAPI'
-import { commentType, PostType } from '@lib/types'
-import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit'
-import axios from 'axios'
+import { commentType, PostType } from "@lib/types";
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
 
 interface PostsState {
-	posts: PostType[]
-	loading: 'idle' | 'pending' | 'succeeded' | 'failed'
-	error: string | null
+  posts: PostType[];
+  loading: "idle" | "pending" | "succeeded" | "failed";
+  error: string | null;
 }
 
 // Initial state
 const initialState: PostsState = {
-	posts: [],
-	loading: 'idle',
-	error: null,
-}
+  posts: [],
+  loading: "idle",
+  error: null,
+};
 
 // Async thunks
-export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
-	const data = await postAPI.fetchPosts();
-	if(data.success) return data.items;
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const response = await axios.get(
+    "https://mindconnect-vebk.onrender.com/api/post/last-posts",
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
+  );
+  return response.data;
 });
+export const addPost = createAsyncThunk(
+  "posts/addPost",
+  async (postData: { title: string; description: string; image: File }) => {
+    const formData = new FormData();
+    formData.append("title", postData.title);
+    formData.append("description", postData.description);
+    formData.append("image", postData.image);
 
-export const addPost = createAsyncThunk('posts/addPost', async (postData: { title: string; description: string; image: File }) => {
-	const data = await postAPI.addPost(postData);
-	if(data.success) return data.newPost;
-});
+    const response = await axios.post(
+      "https://mindconnect-vebk.onrender.com/api/post/add-post",
+      formData,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return response.data;
+  }
+);
 
 export const updatePost = createAsyncThunk(
-	'posts/updatePost',
-	async ({
-		postId,
-		postData,
-	}: {
-		postId: string
-		postData: { title?: string; description?: string; image?: File }
-	}) => {
+  "posts/updatePost",
+  async ({
+    postId,
+    postData,
+  }: {
+    postId: string;
+    postData: { title?: string; description?: string; image?: File };
+  }) => {
+    const formData = new FormData();
+    if (postData.title) formData.append("title", postData.title);
+    if (postData.description)
+      formData.append("description", postData.description);
+    if (postData.image) formData.append("image", postData.image);
 
-		const isPostUpdated = await postAPI.updatePost({ postId, postData });
-		if (isPostUpdated) {
-			return { postId, postData }
-		}
-
-	}
-)
+    const response = await axios.patch(
+      `https://mindconnect-vebk.onrender.com/api/post/update-post/${postId}`,
+      formData,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return response.data;
+  }
+);
 
 export const deletePost = createAsyncThunk(
-	'posts/deletePost',
-	async (postId: string) => {
-		const isPostDeleted = await postAPI.deletePost(postId);
-		return isPostDeleted ? postId : undefined;
-	}
-)
+  "posts/deletePost",
+  async (postId: string) => {
+    await axios.delete(
+      `https://mindconnect-vebk.onrender.com/api/post/delete-post/${postId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    return postId;
+  }
+);
 
 export const addComment = createAsyncThunk(
-	'posts/addComment',
-	async ({ postId, content }: { postId: string; content: string }) => {
-		const data = await postAPI.addComment({ postId, content });
-		if (data.isAdded) {
-			return data.item;
-		}
-	}
-)
+  "posts/addComment",
+  async ({ postId, content }: { postId: string; content: string }) => {
+    const response = await axios.post(
+      `https://mindconnect-vebk.onrender.com/api/post/${postId}/add-comment`,
+      {
+        content,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    return response.data;
+  }
+);
 
 export const fetchComments = createAsyncThunk(
-	'posts/fetchComments',
-	async (postId: string) => {
-		const data = await postAPI.fetchComments(postId);
-		if (data.success) {
-			return data.items;
-		}
-	}
-)
+  "posts/fetchComments",
+  async (postId: string) => {
+    const response = await axios.get(
+      `https://mindconnect-vebk.onrender.com/api/post/${postId}/comments`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+
+    return response.data;
+  }
+);
 
 export const likePost = createAsyncThunk(
-	'posts/likePost',
-	async (postId: string) => {
-		const data = await postAPI.likePost(postId);
-		return data.success && postId;
-	}
-)
+  "posts/likePost",
+  async (postId: string) => {
+    const response = await axios.patch(
+      `https://mindconnect-vebk.onrender.com/api/post/${postId}/like`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    return { postId, message: response.data };
+  }
+);
 
 export const unlikePost = createAsyncThunk(
-	'posts/unlikePost',
-	async (postId: string) => {
-		const data = await postAPI.unlikePost(postId);
-		return data.success && postId;
-	}
-)
+  "posts/unlikePost",
+  async (postId: string) => {
+    const response = await axios.patch(
+      `https://mindconnect-vebk.onrender.com/api/post/${postId}/unlike`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    return { postId, message: response.data };
+  }
+);
 
 // New thunk for fetching a post by ID
 export const fetchPostById = createAsyncThunk(
-	'posts/fetchById',
-	async (postId: string) => {
-		const data = await postAPI.fetchPostById(postId);
-		return data.success && data.item;
-	}
-)
+  "posts/fetchById",
+  async (postId: string) => {
+    const response = await axios.get(
+      `https://mindconnect-vebk.onrender.com/api/post/${postId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    return response.data;
+  }
+);
 
 export const deleteComment = createAsyncThunk(
-	'posts/deleteComment',
-	async ({ postId, commentId }: { postId: string; commentId: string }) => {
-		const data = await postAPI.deleteComment(commentId);
-		return data.success ? {
-			postId, commentId
-		} : undefined
-	}
-)
+  "posts/deleteComment",
+  async ({ postId, commentId }: { postId: string; commentId: string }) => {
+    await axios.delete(
+      `https://mindconnect-vebk.onrender.com/api/post/delete-comment/${commentId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    return { postId, commentId };
+  }
+);
 
 // Slice
 const postsSlice = createSlice({
-	name: 'posts',
-	initialState,
-	reducers: {},
-	extraReducers: builder => {
-		builder
-			.addCase(fetchPosts.pending, state => {
-				state.loading = 'pending'
-			})
-			.addCase(
-				fetchPosts.fulfilled,
-				(state, action: PayloadAction<PostType[]>) => {
-					state.loading = 'succeeded'
-					state.posts = action.payload
-				}
-			)
-			.addCase(fetchPosts.rejected, (state: PostsState, action) => {
-				state.loading = 'failed'
-				state.error = action.error.message || 'Failed to fetch posts'
-			})
-			.addCase(addPost.fulfilled, (state: PostsState, action: PayloadAction<PostType>) => {
-				state.posts.push(action.payload)
-			})
-			.addCase(
-				updatePost.fulfilled,
-				(state: PostsState, action: PayloadAction<{ postId: string; postData: { title?: string; description?: string; image?: File } } | undefined>) => {
-					if (action.payload) {
-						const { postId, postData } = action.payload;
-						const index = state.posts.findIndex(post => post._id === postId);
-						if (index !== -1) {
-							state.posts[index] = { ...state.posts[index], ...postData };
-						}
-					}
-				}
-			)
-			.addCase(deletePost.fulfilled, (state, action: PayloadAction<string | undefined>) => {
-				state.posts = state.posts.filter(post => post._id !== action.payload)
-			})
-			.addCase(addComment.fulfilled, (state, action) => {
-				const post = state.posts.find(
-					post => post._id === action.payload.postId
-				)
-				if (post) {
-					post.comments.push(action.payload)
-				}
-			})
-			.addCase(fetchComments.fulfilled, (state, action) => {
-				state.posts.forEach(post => {
-					post.comments = action.payload.map((comment: commentType) => {
-						if (comment.postId === post._id) {
-							return comment
-						}
-					})
-				})
+  name: "posts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPosts.pending, (state) => {
+        state.loading = "pending";
+      })
+      .addCase(
+        fetchPosts.fulfilled,
+        (state, action: PayloadAction<PostType[]>) => {
+          state.loading = "succeeded";
+          state.posts = action.payload;
+        }
+      )
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.loading = "failed";
+        state.error = action.error.message || "Failed to fetch posts";
+      })
+      .addCase(addPost.fulfilled, (state, action: PayloadAction<PostType>) => {
+        state.posts.push(action.payload);
+      })
+      .addCase(
+        updatePost.fulfilled,
+        (state, action: PayloadAction<PostType>) => {
+          const index = state.posts.findIndex(
+            (post) => post._id === action.payload._id
+          );
+          if (index !== -1) {
+            state.posts[index] = action.payload;
+          }
+        }
+      )
+      .addCase(deletePost.fulfilled, (state, action: PayloadAction<string>) => {
+        state.posts = state.posts.filter((post) => post._id !== action.payload);
+      })
+      .addCase(addComment.fulfilled, (state, action) => {
+        const post = state.posts.find(
+          (post) => post._id === action.payload.postId
+        );
+        if (post) {
+          post.comments.push(action.payload);
+        }
+      })
+      .addCase(fetchComments.fulfilled, (state, action) => {
+        state.posts.forEach((post) => {
+          post.comments = action.payload.map((comment: commentType) => {
+            if (comment.postId === post._id) {
+              return comment;
+            }
+          });
+        });
 
-				const post = state.posts.find(post => post._id == action.payload.postId)
-				console.log(post)
-				if (post) {
-					post.comments = action.payload
-				}
-			})
-			.addCase(likePost.fulfilled, (state, action: PayloadAction<string | false>) => {
-				if (typeof action.payload === 'string') {
-					const post = state.posts.find(
-						post => post._id === action.payload
-					)
-					if (post) {
-						const userId = localStorage.getItem('userId')
-						if (userId && !post.likes.includes(userId)) {
-							post.likes.push(userId)
-						}
-					}
-				}
-			})
-			.addCase(unlikePost.fulfilled, (state, action: PayloadAction<string | false>) => {
-				if (typeof action.payload === 'string') {
-					const post = state.posts.find(
-						post => post._id === action.payload
-					)
-					if (post) {
-						const userId = localStorage.getItem('userId')
-						if (userId) {
-							post.likes = post.likes.filter(id => id !== userId)
-						}
-					}
-				}
-			})
-			.addCase(
-				fetchPostById.fulfilled,
-				(state, action: PayloadAction<PostType>) => {
-					const existingPost = state.posts.find(
-						post => post._id === action.payload._id
-					)
-					if (existingPost) {
-						Object.assign(existingPost, action.payload)
-					} else {
-						state.posts.push(action.payload)
-					}
-				}
-			)
-			.addCase(
-				deleteComment.fulfilled,
-				(
-					state,
-					action: PayloadAction<{ postId: string; commentId: string } | undefined>
-				) => {
-					if (action.payload) {
-						const { postId, commentId } = action.payload
-						const post = state.posts.find(post => post._id === postId)
-						if (post) {
-							post.comments = post.comments.filter(
-								comment => comment !== commentId
-							)
-						}
-					}
-				}
-			)
-	},
-})
+        const post = state.posts.find(
+          (post) => post._id == action.payload.postId
+        );
+        console.log(post);
+        if (post) {
+          post.comments = action.payload;
+        }
+      })
+      .addCase(likePost.fulfilled, (state, action) => {
+        const post = state.posts.find(
+          (post) => post._id === action.payload.postId
+        );
+        if (post) {
+          const userId = localStorage.getItem("userId");
+          if (userId && !post.likes.includes(userId)) {
+            post.likes.push(userId);
+          }
+        }
+      })
+      .addCase(unlikePost.fulfilled, (state, action) => {
+        const post = state.posts.find(
+          (post) => post._id === action.payload.postId
+        );
+        if (post) {
+          const userId = localStorage.getItem("userId");
+          if (userId) {
+            post.likes = post.likes.filter((id) => id !== userId);
+          }
+        }
+      })
+      // Add case for fetchPostById
+      .addCase(
+        fetchPostById.fulfilled,
+        (state, action: PayloadAction<PostType>) => {
+          const existingPost = state.posts.find(
+            (post) => post._id === action.payload._id
+          );
+          if (existingPost) {
+            Object.assign(existingPost, action.payload);
+          } else {
+            state.posts.push(action.payload);
+          }
+        }
+      )
+      .addCase(
+        deleteComment.fulfilled,
+        (
+          state,
+          action: PayloadAction<{ postId: string; commentId: string }>
+        ) => {
+          const { postId, commentId } = action.payload;
+          const post = state.posts.find((post) => post._id === postId);
+          if (post) {
+            post.comments = post.comments.filter(
+              (comment) => comment !== commentId
+            );
+          }
+        }
+      );
+  },
+});
 
-export default postsSlice.reducer
+export default postsSlice.reducer;
